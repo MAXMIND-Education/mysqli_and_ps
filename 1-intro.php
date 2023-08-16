4c3965040dadc0a8646d134d86a666d9e9fcecf6
MAXMIND-Education <139166190+MAXMIND-Education@users.noreply.github.com>
2023-08-17 00:38:41 +0530

    Create 1-intro.php

---
+++ 1-intro.php
@@ -0,0 +1,18 @@
+<?php
+
+
+//Procedural Style:
+//Connection: To connect to a MySQL database using mysqli in procedural style, you can use the following code:
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "database";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+

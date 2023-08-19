7e843984f7833733c5b0f8f60b5a95a78fa02696
MAXMIND-Education <139166190+MAXMIND-Education@users.noreply.github.com>
2023-08-19 13:01:25 +0530

    Update 6-CreateTable.php

--- 6-CreateTable.php
+++ 6-CreateTable.php
@@ -1,34 +1,34 @@
 <?php
 
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "database";
+// $servername = "localhost";
+// $username = "username";
+// $password = "password";
+// $dbname = "database";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+// // Create connection
+// $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+// // Check connection
+// if (!$conn) {
+//     die("Connection failed: " . mysqli_connect_error());
+// }
 
-// SQL query to create a table
-$sql = "CREATE TABLE users (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    firstname VARCHAR(30) NOT NULL,
-    lastname VARCHAR(30) NOT NULL,
-    email VARCHAR(50),
-    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)";
+// // SQL query to create a table
+// $sql = "CREATE TABLE users (
+//     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+//     firstname VARCHAR(30) NOT NULL,
+//     lastname VARCHAR(30) NOT NULL,
+//     email VARCHAR(50),
+//     reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+// )";
 
-if (mysqli_query($conn, $sql)) {
-    echo "Table created successfully";
-} else {
-    echo "Error creating table: " . mysqli_error($conn);
-}
+// if (mysqli_query($conn, $sql)) {
+//     echo "Table created successfully";
+// } else {
+//     echo "Error creating table: " . mysqli_error($conn);
+// }
 
-mysqli_close($conn);
+// mysqli_close($conn);
 
 
 //-------Object-Oriented Style:-----------------------------------------------

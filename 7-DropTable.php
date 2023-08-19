ad51889b37a46b527d2e0f1c964eaf28edfb6a3e
MAXMIND-Education <139166190+MAXMIND-Education@users.noreply.github.com>
2023-08-19 13:05:50 +0530

    Update 7-DropTable.php

--- 7-DropTable.php
+++ 7-DropTable.php
@@ -1,29 +1,29 @@
 <?php
 
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "database";
+// $servername = "localhost";
+// $username = "username";
+// $password = "password";
+// $dbname = "database";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+// // Create connection
+// $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+// // Check connection
+// if (!$conn) {
+//     die("Connection failed: " . mysqli_connect_error());
+// }
 
-// SQL query to drop a table
-$tableName = "table_to_drop";
-$sql = "DROP TABLE $tableName";
+// // SQL query to drop a table
+// $tableName = "table_to_drop";
+// $sql = "DROP TABLE $tableName";
 
-if (mysqli_query($conn, $sql)) {
-    echo "Table dropped successfully";
-} else {
-    echo "Error dropping table: " . mysqli_error($conn);
-}
+// if (mysqli_query($conn, $sql)) {
+//     echo "Table dropped successfully";
+// } else {
+//     echo "Error dropping table: " . mysqli_error($conn);
+// }
 
-mysqli_close($conn);
+// mysqli_close($conn);
 
 
 //----Object-Oriented Style:-------------------------------------------------------

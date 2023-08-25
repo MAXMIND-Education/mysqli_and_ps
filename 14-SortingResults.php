0a66f40b0c2083fe9441fb94fa939759bb335f76
MAXMIND-Education <139166190+MAXMIND-Education@users.noreply.github.com>
2023-08-25 12:49:53 +0530

    Create 14-SortingResults.php

---
+++ 14-SortingResults.php
@@ -0,0 +1,62 @@
+<?php
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "database";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// SQL query with an ORDER BY clause
+$sql = "SELECT id, firstname, lastname FROM users ORDER BY lastname, firstname";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "ID: " . $row["id"] . " - Name: " . $row["firstname"] . " " . $row["lastname"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+
+
+//---------Object-Oriented Style:-----------------------------
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// SQL query with an ORDER BY clause
+$sql = "SELECT id, firstname, lastname FROM users ORDER BY lastname, firstname";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["id"] . " - Name: " . $row["firstname"] . " " . $row["lastname"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+
+
+//----------ORDER BY lastname DESC------------------
+//-----------ORDER BY lastname DESC, firstname ASC-------------------------
